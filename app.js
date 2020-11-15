fda7f950389276c992f8f48a6f83fa8d7d879a09
Mark Adkins <mark@bymark.co>
2020-11-15 02:19:40 -0500

    Authentication Complete

--- app.js
+++ app.js
@@ -1,12 +1,86 @@
+import * as fs from "fs";
+import * as path from "socket.io";
+
 const app = require('express')();
+const session = require('express-session');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const { PeerServer } = require('peer');
 const UIDGenerator = require('uid-generator');
 const uidgen = new UIDGenerator(256);
+require('dotenv').config()
+const { v4: uuidv4 } = require('uuid');
+const { google } = require('googleapis');
+const OAuth2Data = require('./google-client-credentials.json');
+const environment = process.env.NODE_ENV;
+const queryString = require('query-string');
+
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URL = (environment === "development")? OAuth2Data.web.redirect_uris[0] : OAuth2Data.web.redirect_uris[1];
+
+const OAuth2Client = createConnection();
+const scopes = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+];
+
+let instructors = require('./data/instructors.json');
+
+app.set('trust proxy', 1)
+app.use(session({
+    secret: process.env.APPSECRET,
+    resave: true,
+    saveUninitialized: true,
+    genid: function(req) {
+        return uuidv4(); // use UUIDs for session IDs
+    },
+    cookie: {
+        maxAge: 24 * 60 * 60 * 365 * 1000
+    }
+}));
 
 let classrooms = new Map();
 const peerServer = PeerServer({ port: 9000, path: '/' });
+
+function createConnection() {
+    return new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+}
+
+function generateToken() {
+    require('crypto').randomBytes(48, function(err, buffer) {
+        return buffer.toString('hex');
+    });
+}
+
+function authenticate(req) {
+    if (!req.session.authed) {
+        // Generate an OAuth URL and redirect there
+        const url = OAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: scopes,
+        });
+        return {
+            redirect: true,
+            url: url,
+            message: 'Redirecting'
+        };
+    } else {
+        return {
+            redirect: false,
+            message: 'Logged in'
+        };
+    }
+}
+
+function saveClassroomRedirect(request, classroomID, instructor = false) {
+    request.session.redirect = {classroom: classroomID, instructor: instructor}
+}
+
+function saveProfileToSession(request, profile) {
+    request.session.profile = profile;
+    request.session.save()
+}
 
 function createClassroomID(iteration = 1) {
     let id = generateID();
@@ -16,6 +90,51 @@ function createClassroomID(iteration = 1) {
     } else {
         return id;
     }
+}
+
+function fileExists(path) {
+    try {
+        if (fs.existsSync(path)) {
+            return true;
+        }
+    } catch(err) {
+        return false;
+    }
+}
+
+function generateUniqueFilename(parentPath, extension) {
+    let name = generateToken(48);
+    if(fileExists(parentPath + name + extension)) {
+        return generateUniqueFilename(parentPath, extension);
+    }
+    return name + extension;
+}
+
+function getFileExtension(filename) {
+    return filename.split('.').pop();
+}
+
+function isInstructor(profile) {
+    if(profile) {
+        for(let i = 0;i < profile.emails.length; i++) {
+            if(instructors.includes(profile.emails[i].value)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function isOwner(profile, classroomID) {
+    if(profile && isInstructor(profile)) {
+        let instructorEmail = classrooms.get(classroomID).email;
+        for(let i = 0;i < profile.emails.length; i++) {
+            if(instructorEmail === profile.emails[i].value) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 function generateID() {
@@ -31,16 +150,18 @@ function generateID() {
     return result;
 }
 
-function createClassroom(teacher, className) {
+function createClassroom(profile, teacher, className) {
     let roomID = createClassroomID();
     classrooms.set(roomID, {
         teacher: teacher,
+        email: profile.emails[0].value,
         class: className,
         token: uidgen.generateSync(),
+        open: false,
+        media: {},
         attendees: 0
     });
 
-    console.log('Classroom created.  ID:', roomID)
     return roomID;
 }
 
@@ -48,8 +169,37 @@ function deleteClassroom(roomID) {
     classrooms.delete(roomID);
 }
 
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function getUserData(client, request) {
+    const service = google.people({version: 'v1', auth: client});
+    service.people.get({
+        resourceName: "people/me",
+        personFields: "names,emailAddresses"
+    }, (err, res) => {
+        if (err) return console.error('The API returned an error: ' + err);
+        const profile = res.data;
+        if (profile) {
+            for(let i = 0; i < profile.emailAddresses.length; i++) {
+                delete profile.emailAddresses[i].metadata;
+            }
+            saveProfileToSession(request, {
+                name: profile.names[0].displayName,
+                emails: profile.emailAddresses
+            });
+        }
+    });
+}
+
 app.get('/', (req, res) => {
-    res.send('<p>App Home Page</p>');
+    if(req.session.redirect) {
+        if(req.session.redirect.instructor) {
+            res.redirect('/i/' + req.session.redirect.classroom)
+        } else {
+            res.redirect('/c/' + req.session.redirect.classroom)
+        }
+    } else {
+        res.send('<p>App Home Page</p>');
+    }
 });
 
 app.get('/js/socket.io.js', (req, res) => {
@@ -60,16 +210,63 @@ app.get('/js/*', (req, res) => {
     res.sendFile(__dirname + '/src' + req.path);
 });
 
+app.get('/media/*', (req, res) => {
+    res.sendFile(__dirname + '/uploads' + req.path);
+});
+
+// Remove this once files can be uploaded
 app.get('/uploads/*', (req, res) => {
     res.sendFile(__dirname + '/src' + req.path);
 });
 
+app.get('/auth', function (req, res) {
+    const code = req.query.code
+    let request = req;
+    if (code) {
+        // Get an access token based on our OAuth code
+        OAuth2Client.getToken(code, function (err, tokens) {
+            if (err) {
+                console.err(err);
+            } else {
+                OAuth2Client.setCredentials(tokens);
+                request.session.tokens = tokens;
+                getUserData(OAuth2Client, request);
+                req.session.authed = true;
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+app.get('/signout', function (req, res) {
+    if(req.session.authed && req.session.authed === true) {
+        req.session.authed = false;
+        req.session = {};
+        req.session.save();
+    }
+    res.redirect('/');
+});
+
 app.get('/c/*/', (req, res) => {
+    if(req.session.redirect) {
+        delete req.session.redirect;
+    }
     let classID = req.path.split('/')[2];
     if(classrooms.has(classID)) {
-        let classData = classrooms.get(classID);
-        res.status(200);
-        res.sendFile(__dirname + '/src/classroom.html');
+        let auth = authenticate(req);
+        if(auth.redirect) {
+            saveClassroomRedirect(req, classID, false);
+            res.redirect(auth.url);
+        } else {
+            let classData = classrooms.get(classID);
+            if(classData.open === true) {
+                res.status(200);
+                res.sendFile(__dirname + '/src/classroom.html');
+            } else {
+                res.status(200);
+                res.send('<p>The instructor has not opened the room yet.  Please try again later.</p>');
+            }
+        }
     } else {
         res.status(404);
         res.send("<p>Not Found</p>")
@@ -77,63 +274,189 @@ app.get('/c/*/', (req, res) => {
 });
 
 app.get('/i/*/', (req, res) => {
+    if(req.session.redirect) {
+        delete req.session.redirect;
+    }
     let classID = req.path.split('/')[2];
     if(classrooms.has(classID)) {
-        let classData = classrooms.get(classID);
-        res.status(200);
-        res.sendFile(__dirname + '/src/instructor.html');
+        let auth = authenticate(req);
+        if(auth.redirect) {
+            saveClassroomRedirect(req, classID, true);
+            res.redirect(auth.url);
+        } else {
+            if(isOwner(req.session.profile, classID)) {
+                let classData = classrooms.get(classID);
+                res.status(200);
+                res.sendFile(__dirname + '/src/instructor.html');
+            } else {
+                res.status(403);
+                res.send("<h1>Not Authorized</h1><p>You are not an instructor of this class.  Please make sure you are signed into the correct account.</p>")
+            }
+        }
     } else {
         res.status(404);
         res.send("<p>Not Found</p>")
     }
 });
 
-app.get('/api/create/', (req, res) => {
-    let teacher = req.query.teacher;
-    let className = req.query.class;
-    if(!teacher || !className) {
-        res.status(400);
-        res.json({
-            status: 400,
-            error: 'Both the `teacher` and `class` attributes are required.'
-        });
+app.get('/signin/', (req, res) => {
+    if(req.session.redirect) {
+        delete req.session.redirect;
+    }
+    let auth = authenticate(req);
+    if(auth.redirect) {
+        res.redirect(auth.url);
     } else {
-        res.status(200);
-        res.json({
-            status: 200,
-            teacher: teacher,
-            class: className,
-            message: 'A new classroom has been created for ' + teacher + '\'s ' + className + ' class.',
-            id: createClassroom(teacher, className)
-        });
-        console.log(classrooms);
+        res.redirect('/');
+    }
+});
+
+app.get('/api/create/', (req, res) => {
+    if(isInstructor(req.session.profile)) {
+        let teacher = req.session.profile.name;
+        let className = req.query.class;
+        if(!teacher || !className) {
+            res.status(400);
+            res.json({
+                status: 400,
+                error: 'The `class` attribute is required.'
+            });
+        } else {
+            res.status(200);
+            res.json({
+                status: 200,
+                teacher: teacher,
+                class: className,
+                message: 'A new classroom has been created for ' + teacher + '\'s ' + className + ' class.',
+                id: createClassroom(req.session.profile, teacher, className)
+            });
+        }
+    } else {
+        res.status(403);
+        res.send("Not Authorized");
     }
 });
 
 app.get('/api/delete/', (req, res) => {
     let classID = req.query.classID;
-    if(!classID) {
-        res.status(400);
-        res.json({
-            status: 400,
-            error: 'The `classID` attribute is required.'
-        });
-    } else if(!classrooms.has(classID)) {
-        res.status(400);
-        res.json({
-            status: 400,
-            error: 'Cannot delete classroom.  The classroom ID (' + classID + ') does not exist.'
-        });
+    if(isOwner(req.session.profile, classID)) {
+        if(!classID) {
+            res.status(400);
+            res.json({
+                status: 400,
+                error: 'The `classID` attribute is required.'
+            });
+        } else if(!classrooms.has(classID)) {
+            res.status(400);
+            res.json({
+                status: 400,
+                error: 'Cannot delete classroom.  The classroom ID (' + classID + ') does not exist.'
+            });
+        } else {
+            let classData = classrooms.get(classID);
+            deleteClassroom(classID);
+            res.status(200);
+            res.json({
+                status: 200,
+                teacher: classData.teacher,
+                class: classData.class,
+                message: 'The classroom for ' + classData.teacher + '\'s ' + classData.class + ' class with the ID ' + classID + ' has been deleted.'
+            });
+        }
     } else {
+        res.status(403);
+        res.send("Not Authorized");
+    }
+});
+
+app.get('/api/classes/', (req, res) => {
+    if(isInstructor(req.session.profile)) {
+        let myClasses = [];
+        for (const [classID, value] of classrooms.entries()) {
+            for(let i = 0; i < req.session.profile.emails.length; i++) {
+                if(req.session.profile.emails[i].value === value.email) {
+                    let classFound = classrooms.get(classID);
+                    classFound.classID = classID;
+                    myClasses.push(classFound);
+                }
+            }
+        }
+        res.status(200);
+        res.json(myClasses);
+    } else {
+        res.status(403);
+        res.send("Not Authorized");
+    }
+});
+
+app.get('/api/open/', (req, res) => {
+    let classID = req.query.classID;
+    if(isOwner(req.session.profile, classID)) {
         let classData = classrooms.get(classID);
-        deleteClassroom(classID);
+        classData.open = true;
+        classrooms.set(classID, classData);
         res.status(200);
         res.json({
             status: 200,
-            teacher: classData.teacher,
-            class: classData.class,
-            message: 'The classroom for ' + classData.teacher + '\'s ' + classData.class + ' class with the ID ' + classID + ' has been deleted.'
+            message: "The classroom is now open."
         });
+    } else {
+        res.status(403);
+        res.send("Not Authorized");
+    }
+});
+
+app.get('/api/close/', (req, res) => {
+    let classID = req.query.classID;
+    if(isOwner(req.session.profile, classID)) {
+        let classData = classrooms.get(classID);
+        classData.open = false;
+        classrooms.set(classID, classData);
+        res.status(200);
+        res.json({
+            status: 200,
+            message: "The classroom has been closed."
+        });
+    } else {
+        res.status(403);
+        res.send("Not Authorized");
+    }
+});
+
+app.post('/api/upload/', (req, res) => {
+    let classID = req.query.classID;
+    if(isOwner(req.session.profile, classID)) {
+        if (!req.files || Object.keys(req.files).length === 0) {
+            res.status(400);
+            res.send('No files were submitted.');
+        }
+        let mediaFile = req.files.media;
+
+        let filename = generateUniqueFilename(path.join(__dirname, 'uploads'), getFileExtension(mediaFile.name))
+        mediaFile.mv(path.join(__dirname, 'uploads', filename), function(err) {
+            if (err) {
+                res.status(500);
+                res.send("The file failed to upload");
+            }
+        });
+
+        let classData = classrooms.get(classID);
+        classData.media.push('/media/' + filename);
+        classrooms.set(classID, classData);
+
+        res.status(200);
+        res.json({ status: 200, message: "The file has been added to the lesson plan." });
+    }
+});
+
+app.get('/api/profile/', (req, res) => {
+    if(req.session.profile) {
+        let profile = req.session.profile;
+        res.status(200);
+        res.json(profile);
+    } else {
+        res.status(403);
+        res.json({});
     }
 });
 
