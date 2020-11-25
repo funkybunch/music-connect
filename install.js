7220f09101a9ada30e56d165d6def57402f26a1a
Mark Adkins <mark@bymark.co>
2020-11-24 21:01:50 -0500

    Create Install Script

--- install.js
+++ install.js
@@ -1,5 +1,27 @@
 const common = require('./common.js');
 
-if(!common.fileExists(path.join(__dirname, 'data', 'instructors.json'))) {
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios').default;
+const env = require('dotenv').config();
+const { spawn } = require('child_process');
 
+// Functions
+function buildApp() {
+    spawnProcess('npm', ['run', 'build']);
 }
+
+function spawnProcess(cmd, args) {
+    const process = spawn(cmd, args, {stdio: ['inherit']});
+    process.stdout.on('data', function(data) {
+        console.log(data.toString());
+    });
+    process.stderr.on('data', function(data) {
+        console.log('stderr: ' + data.toString());
+    });
+    process.on('exit', function(code) {
+        console.log('child process exited with code ' + code.toString());
+    })
+}
+
+buildApp()
